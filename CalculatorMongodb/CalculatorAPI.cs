b5f644ab66acc50962fc4d25d576628ac9a21d00
Shravya2727 <shravyaise27@gmail.com>
2023-05-31 18:53:33 +0530

    Add project files.

---
+++ CalculatorMongodb/CalculatorAPI.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CalculatorMongodb
+{
+    internal class CalculatorAPI
+    {
+        private readonly IMongoCollection<CalculationHistory> collection;
+
+
+        public CalculatorAPI()
+        {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+            IMongoDatabase database = client.GetDatabase("calculatorMongoDB");
+            collection = database.GetCollection<CalculationHistory>("history");
+
+        }
+
+        public double OperationPerform(double firstNumber, double secondNumber, string operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    throw new ArgumentException("Enter valid operation");
+                   
+
+            }
+            CalculationHistory history = new CalculationHistory
+            {
+                FirstNumber = firstNumber,
+                SecondNumber = secondNumber,
+                Operation = operation,
+                Result = result,
+                CreatedOn = DateTime.Now
+
+            };
+
+            collection.InsertOne(history);
+            return result;
+        }
+    }
+
+    class CalculationHistory
+    { 
+    public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public string Operation{ get; set; }
+        public double Result { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+
+    }
+}
---
+++ CalculatorMongodb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMongodb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your choice(+, -, *, /): ");
+            string operation= Console.ReadLine();
+
+
+            //Make API Call
+            CalculatorAPI calculatorAPI = new CalculatorAPI();
+            double result = calculatorAPI.OperationPerform(firstNumber,secondNumber,operation);
+
+            Console.WriteLine("Result: " +result);
+
+            Console.ReadKey();
+                
+
+        }
+    }
+}
